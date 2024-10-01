df1815ab896462496c7dd3c21f0721e61bf8e61d
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-09-30 20:03:01 -0400

    added an target

--- ScavengerHunt/ViewModels/TargetsViewModel.swift
+++ ScavengerHunt/ViewModels/TargetsViewModel.swift
@@ -35,6 +35,16 @@ class TargetsViewModel {
             completed: false
         ),
         
+        TargetRegion(
+                    latitude: 44.43775626,
+                    longitude: -78.26757131,
+                    radius: 8,
+                    identifier: "Sign infront of thw House",
+                    question: "What is the 3rd newest house built on campus?",
+                    answer: "Cooper House",
+                    completed: false
+                ),
+        
 
 
     ]
